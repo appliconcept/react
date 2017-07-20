0b8ae97b83916aa2c5ca74b484418a895d6502a6
Appliconcept <contact@appliconcept.com>
2017-07-20 22:17:43 +0400

    src problem

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import Button from "./src/components/Button";
-import Checkbox from "./src/components/Checkbox";
-import CheckboxGroup from "./src/components/CheckboxGroup";
-import Column from "./src/components/Column.js";
-import Container from "./src/components/Container";
-import Form from "./src/components/Form";
-import Icon from "./src/components/Icon";
-import Input from "./src/components/Input";
-import Radio from "./src/components/Radio";
-import RadioGroup from "./src/components/RadioGroup";
-import Row from "./src/components/Row";
-import Submit from "./src/components/Submit";
+import Button from "./components/Button";
+import Checkbox from "./components/Checkbox";
+import CheckboxGroup from "./components/CheckboxGroup";
+import Column from "./components/Column.js";
+import Container from "./components/Container";
+import Form from "./components/Form";
+import Icon from "./components/Icon";
+import Input from "./components/Input";
+import Radio from "./components/Radio";
+import RadioGroup from "./components/RadioGroup";
+import Row from "./components/Row";
+import Submit from "./components/Submit";
 
 export {
     Button,
